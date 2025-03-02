30f1dd3571617d6881852981c929e2ec26ffe50e
priyanka-r <139608997+priyankar-r@users.noreply.github.com>
2025-03-03 00:04:23 +0530

    Delete week 5 game store

--- week 5 game store/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Text } from "@chakra-ui/react";
-function Footer() {
-  return (
-    <Box bg="#ed7412" color="white" py={4} textAlign="center" mt={10}>
-      <Text>© {new Date().getFullYear()} Game Store. All Rights Reserved.</Text>
-    </Box>
-  );
-}
-export default Footer;
